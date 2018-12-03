145d0d008892a27f0d2a2be85c7d31a1bc2b27f8
Edward Brennan <EdMan1022@gmail.com>
2018-12-03 11:38:57 -0500

    Add test for day2 part one

--- src/tests/test_week_1.py
+++ src/tests/test_week_1.py
@@ -1,22 +1,42 @@
-from unittest import TestCase
+from unittest import TestCase, main
 
-from src.main.week1 import day1
+from src.main import week1
 
 
 class TestDay1(TestCase):
 
     def test_ex_1(self):
         test_data = [1, -1]
-        self.assertEqual(day1(test_data), 0)
+        self.assertEqual(week1.day1(test_data), 0)
     
     def test_ex_2(self):
         test_data = [3, 3, 4, -2, -4]
-        self.assertEqual(day1(test_data), 10)
+        self.assertEqual(week1.day1(test_data), 10)
 
     def test_ex_3(self):
         test_data = [-6, 3, 8, 5, -6]
-        self.assertEqual(day1(test_data), 5)
+        self.assertEqual(week1.day1(test_data), 5)
 
     def test_ex_4(self):
         test_data = [7, 7, -2, -7, -4]
-        self.assertEqual(day1(test_data), 14)
+        self.assertEqual(week1.day1(test_data), 14)
+
+class TestDay2(TestCase):
+
+    def test_ex_1(self):
+
+        test_data = [
+            'abcdef',
+            'bababc',
+            'abbcde',
+            'abcccd',
+            'aabcdd',
+            'abcdee',
+            'ababab'
+            ]
+        
+        self.assertEqual(week1.day2(test_data), 12)
+
+
+if __name__ == '__main__':
+    main()
