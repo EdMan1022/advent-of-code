d5801a925f4a1b1a538aafd1c91d8531df4a0205
Edward Brennan <EdMan1022@gmail.com>
2018-12-03 10:45:31 -0500

    Add 0th text file for 0th exercize, complete first exercize

--- src/main/week1.py
+++ src/main/week1.py
@@ -1,16 +1,43 @@
+from itertools import cycle
+import os
+
+
+def day0(data):
+    return sum(data)
+
+
 def day1(data):
-    return sum([int(_) for _ in data])
 
+    cached_sums = set([0])
+    _sum = 0
 
-def day2(data):
-    return None
+    for value in cycle(data):
+
+        _sum = value + _sum
+
+        if _sum in cached_sums:
+            return _sum
+    
+        cached_sums.add(_sum)
 
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
         data = file.readlines()
-    return data
+    return [float(_) for _ in data]
 
 
 if __name__ == '__main__':
-    print(day1(load_data('inputs/day1.txt')))
+
+    root = os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.abspath(__file__)
+            )
+        )
+    )
+
+    print(day1(
+        load_data('{}/inputs/day1.txt'.format(root))
+        ))
+
--- src/tests/test_week_1.py
+++ src/tests/test_week_1.py
@@ -1,22 +1,22 @@
 from unittest import TestCase
 
-from src.main.week1 import day2
+from src.main.week1 import day1
 
 
-class TestDay2(TestCase):
+class TestDay1(TestCase):
 
     def test_ex_1(self):
         test_data = [1, -1]
-        self.assertEqual(day2(test_data), 0)
+        self.assertEqual(day1(test_data), 0)
     
     def test_ex_2(self):
         test_data = [3, 3, 4, -2, -4]
-        self.assertEqual(day2(test_data), 10)
+        self.assertEqual(day1(test_data), 10)
 
     def test_ex_3(self):
         test_data = [-6, 3, 8, 5, -6]
-        self.assertEqual(day2(test_data), 5)
+        self.assertEqual(day1(test_data), 5)
 
     def test_ex_4(self):
         test_data = [7, 7, -2, -7, -4]
-        self.assertEqual(day2(test_data), 14)
+        self.assertEqual(day1(test_data), 14)
