4cc943f0ce01beda418bf28cabc93ad05237b9b8
Edward Brennan <EdMan1022@gmail.com>
2018-12-03 11:39:10 -0500

    Implement day 2 part 1

--- src/main/week1.py
+++ src/main/week1.py
@@ -20,11 +20,39 @@ def day1(data):
     
         cached_sums.add(_sum)
 
+def day2(data):
+
+    counts = {
+        1: 0,
+        2: 0,
+        3: 0
+    }
+
+    for id_string in data:
+        sub_counts = {
+            1: False,
+            2: False,
+            3: False
+        }
+        char_set = set(id_string)
+        for character in char_set:
+            sub_counts[id_string.count(character)] = True
+        
+        for key, value in sub_counts.items():
+            if value:
+                counts[key] += 1
+    
+    return counts[2] * counts[3]
+
+
+def load_int_data(filepath):
+    data = load_data(filepath)
+    return [float(_) for _ in data]
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
         data = file.readlines()
-    return [float(_) for _ in data]
+    return data
 
 
 if __name__ == '__main__':
@@ -37,7 +65,7 @@ if __name__ == '__main__':
         )
     )
 
-    print(day1(
-        load_data('{}/inputs/day1.txt'.format(root))
+    print(day2(
+        load_data('{}/inputs/day2.txt'.format(root))
         ))
 
